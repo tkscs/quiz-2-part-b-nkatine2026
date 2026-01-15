25902cdf9022630a31795213c190bbf5a4b6f1c1
nkatine2026 <nkatine2026@kehillahstudent.org>
2026-01-15 09:08:36 -0800

    done

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-
+def game():
+    state = "FOREST"
+    w = input("Would you like to go North or East?")
+    if w == "North":
+        state = "CAVE"
+        print("What is this??? TREASURE??? YOU WON!!!")
+    elif w == "East":
+        state = "RIVER"
+        s = input("You are at a river, would you like to swim or return to the forest?")
+        if s == "swim":
+            print("YOU DROWNED!!! WHAT ARE YOU DOING?!?!")
+        elif s == "return to forest":
+            state = "FOREST"
+            return game()
+    else: 
+        print("YOU IDIOT THERE IS A BEAR IN THAT DIRECTION!!!")
+game()
